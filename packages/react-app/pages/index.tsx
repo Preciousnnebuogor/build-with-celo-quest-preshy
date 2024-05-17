fe276874f70be8436f0e2849de9495d878ade95d
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-05-17 19:22:51 +0100

    added minipay code snippet

---
+++ packages/react-app/pages/index.tsx
@@ -0,0 +1,58 @@
+eaimport { useEffect, useState } from "react";
+import { useAccount } from "wagmi";
+
+export default function Home() {
+  const [userAddress, setUserAddress] = useState("");
+  const [isMounted, setIsMounted] = useState(false);
+  const { address, isConnected } = useAccount();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (isConnected && address) {
+      setUserAddress(address);
+    }
+    checkForMinipay();
+  }, [address, isConnected]);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  async function checkForMinipay() {
+    // The code must run in a browser environment and not in node environment
+    if (window && window.ethereum) {
+      // User has a injected wallet
+
+      if (window.ethereum.isMinipay) {
+        // User is using Minipay
+
+        // Requesting account addresses
+        let accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+          params: [],
+        });
+
+        // Injected wallets inject all available addresses,
+        // to comply with API Minipay injects one address but in the form of array
+        console.log(accounts[0]);
+      }
+
+      // User is not using MiniPay
+    }
+  }
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="h1">
+        There you go... a canvas for your next Celo project!
+      </div>
+      {isConnected ? (
+        <div className="h2 text-center">Your address: {userAddress}</div>
+      ) : (
+        <div>No Wallet Connected</div>
+      )}
+    </div>
+  );
+}
